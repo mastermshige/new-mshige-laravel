594decff81eb710cf45421473327930ecc619c88
mshige <mshige_0902@kfd.biglobe.ne.jp>
2023-08-23 23:30:34 +0900

    08232330 commit from iMac

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::resource('post', PostController::class);
 
 Route::get('/', function () {
     return view('welcome');
