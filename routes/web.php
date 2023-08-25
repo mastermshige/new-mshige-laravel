7c7d03c32eb85c272f189bc2425cd124a4bdedcc
mshige <mshige_0902@kfd.biglobe.ne.jp>
2023-08-25 19:10:40 +0900

    gate-policy commit from iMac

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'can:admin'])->group(function () {
     Route::get('profile/index', [ProfileController::class, 'index'])->name('profile.index');
+});
+
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
